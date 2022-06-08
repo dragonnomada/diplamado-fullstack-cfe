184112f0d40d51b13b709e90957a44a085416d04
GILBERTO MUNGUIA VENTURA <9eal8@cfe.mx>
2022-06-08 10:30:48 -0700

    Proyecto Final GMV

---
+++ proyecto_final/proyecto_final_GMV/services/db/index.js
@@ -0,0 +1,35 @@
+const mysql2 = require("mysql2/promise")
+
+const options = {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DBNAME,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+}
+
+let connection = null
+
+async function connect() {
+    connection = await mysql2.createConnection(options)
+}
+
+async function disconnect() {
+    await connection.end()
+    connection = null
+}
+
+function getConnection() {
+    return connection
+}
+
+function isConnected() {
+    return connection !== null
+}
+
+module.exports = {
+    connect,
+    disconnect,
+    getConnection,
+    isConnected
+}
