bdb75ab44b11dec6d1c6bbeb3c80f42e4f9dc959
LUIS GERARDO HIGUERA PEREZ <9NCC6@cfe.mx>
2022-06-28 19:52:25 -0700

    Proyecto Final M2 LGHP

---
+++ proyecto_final/proyecto-final-m2-lghp/index.js
@@ -0,0 +1,40 @@
+//require("dotenv").config()
+
+const http = require("http")
+const express = require("express")
+
+//const dbService = require("./services/db")
+const cfeContactoRouter = require("./routers/cfe/contacto")
+const cfeRecibosRouter = require("./routers/cfe/recibos")
+
+async function main() {
+
+    console.log("Estableciendo conexión a la base de datos...")
+
+  //  await dbService.connect()
+
+    console.log("Se ha establecido la conexión a la base de datos")
+
+    const app = express()
+
+		app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
+
+
+    // TODO: RUTAS
+    app.use("/api/cfe/contacto", cfeContactoRouter)
+    app.use("/api/cfe/recibos", cfeRecibosRouter)
+
+    app.use("/", express.static("public"))
+
+    const server = http.createServer(app)
+
+    console.log("Servidor iniciado un servidor en http://localhost:5000/")
+
+    server.listen(5000)
+
+}
+
+main().catch(error => {
+    console.log(`${error}`)
+})
