ff5e66c98c59dd1d4b6705f3e8f2478595e6e3ca
vicmanglezarv <vicman97@hotmail.com>
2022-06-28 20:55:26 -0700

    Proyecto Final M2 VMGA

---
+++ proyecto_final_modulo_2/proyecto-final-m2-vmga/index.js
@@ -0,0 +1,48 @@
+// index.js
+
+// Se desactiva para este proyecto por: VMGA
+// require("dotenv").config()
+
+const http = require("http")
+const express = require("express")
+
+// Se desactiva para este proyecto ya que no se va a trabajar con base de datos por: VMGA
+// const dbService = require("./services/db")
+
+const cfeRecibosRouter = require("./routers/cfe/recibos")
+const cfeContactoRouter = require("./routers/cfe/contacto")
+
+async function main() {
+
+    console.log("Estableciendo conexión a la base de datos...")
+
+    // Se desactiva para este proyecto ya que no se va a trabajar con base de datos por: VMGA
+    // await dbService.connect()
+
+    console.log("Se ha establecido la conexión a la base de datos")
+
+    const app = express()
+
+		app.use(express.json())
+    
+    app.use(express.urlencoded({ extended: true })) 
+
+    // TODO: RUTAS
+    app.use("/", express.static("public"))
+
+    app.use("/api/cfe/contacto", cfeContactoRouter)
+
+    app.use("/api/cfe/recibos", cfeRecibosRouter)
+    
+
+    const server = http.createServer(app)
+
+    console.log("Servidor iniciado un servidor en http://localhost:5000/")
+
+    server.listen(5000)
+
+}
+
+main().catch(error => {
+    console.log(`${error}`)
+})
