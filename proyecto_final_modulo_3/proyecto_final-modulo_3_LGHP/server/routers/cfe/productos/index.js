b86fd7e803af558cacebd001431542ad2646a95d
LUIS GERARDO HIGUERA PEREZ <9NCC6@cfe.mx>
2022-07-18 12:14:50 -0700

    Proyecto Final M3 LGHP

---
+++ proyecto_final_modulo_3/proyecto_final-modulo_3_LGHP/server/routers/cfe/productos/index.js
@@ -0,0 +1,18 @@
+// routers/cfe/productos/index.js
+const express = require("express")
+
+const router = express.Router()
+
+const productos = []
+
+router.put("/nuevo", (request, response) => {
+	const producto = request.body
+	productos.push(producto)
+	response.send("ok")
+})
+
+router.get("/", (request, response) => {
+	response.send(productos)
+})
+
+module.exports = router
