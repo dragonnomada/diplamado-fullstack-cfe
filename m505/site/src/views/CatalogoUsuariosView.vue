3a0d48dd69279ca0c18a20e4681861619c47e025
dragonnomada <dragonnomada123@gmail.com>
2022-09-05 12:14:19 -0500

    m505

--- m505/site/src/views/CatalogoUsuariosView.vue
+++ m505/site/src/views/CatalogoUsuariosView.vue
@@ -2,13 +2,30 @@
     <div>
         <h1>Nuevo Usuario</h1>
         <div>
-            <input type="text" placeholder="Nombre">
-            <input type="email" placeholder="Correo">
-            <input type="contraseña" placeholder="Contraseña">
-            <button>Crear</button>
+            <input type="text" placeholder="Nombre" v-model="nombre">
+            <input type="email" placeholder="Correo" v-model="correo">
+            <input type="contraseña" placeholder="Contraseña" v-model="contraseña">
+            <button @click="crearAnomalia()">Crear</button>
         </div>
     </div>
     <!-- TODO: Ver los usuarios registrados -->
     <!-- TODO: Editar un usuario -->
     <!-- TODO: Eliminar un usuario -->
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+// TODO: Crear una función en el servicio
+// que envíe los datos de la anomalía al server
+import { fetchNuevoUsuario } from "../services/anomalias"
+
+const nombre = ref("")
+const correo = ref("")
+const contraseña = ref("")
+
+async function crearAnomalia() {
+    await fetchNuevoUsuario(nombre, correo, contraseña)
+}
+
+</script>
